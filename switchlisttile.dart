aa9f2e14badf6fc1e68c3aab962b40716699cd86
THADAKALAVANNURVALI <51965273+thadakalavannurvali@users.noreply.github.com>
2021-05-11 13:53:58 +0530

    Create switchlisttile.dart

---
+++ switchlisttile.dart
@@ -0,0 +1,54 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool _lights = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SwitchListTile(
+        title: const Text('Lights'),
+        value: _lights,
+        onChanged: (bool value) {
+          setState(() {
+            _lights = value;
+          });
+        },
+        secondary: const Icon(Icons.lightbulb_outline),
+      ),
+    );
+  }
+}
